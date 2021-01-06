a1f0a2a6632e79f67159c1c202709d0a5007aec5
HxterNinja <mattschrubb@gmail.com>
2021-01-05 23:02:46 -0600

    Comment out Gui references

--- src/client/StarterPlayerScripts/ClientMain/init.client.lua
+++ src/client/StarterPlayerScripts/ClientMain/init.client.lua
@@ -1,15 +1,15 @@
 local ClientData = require(game.ReplicatedStorage:WaitForChild("ClientData"))
 
 local Player = game.Players.LocalPlayer
-local CashGui = Player.PlayerGui:WaitForChild("ScreenGui"):WaitForChild("Frame"):WaitForChild("Cash")
+--local CashGui = Player.PlayerGui:WaitForChild("ScreenGui"):WaitForChild("Frame"):WaitForChild("Cash")
 
 
 
 local player_profile = ClientData:GetLocalPlayerProfile()
 local player_data = player_profile.Data or ClientData:GetLocalPlayerData() -- Either way will give you the player's data
 
-CashGui.Text = "Cash: " .. player_data.Cash
+--CashGui.Text = "Cash: " .. player_data.Cash
 player_profile:ListenToChange("Cash", function(new_value, old_value)
 	print(game.Players.LocalPlayer.Name .. " Cash changed: " .. old_value .. " -> " .. player_data.Cash or new_value)
-	CashGui.Text = "Cash: " .. new_value
+	--CashGui.Text = "Cash: " .. new_value
 end)
